b02a0bb8b65d928982ae8e222b0ba18d5af16077
nickbui729 <38127597+nickbui729@users.noreply.github.com>
2019-10-14 14:23:13 -0700

    added files

---
+++ ReactJs Burger Builder/build/precache-manifest.fa44ebf31ac92678144fd40b72ffc7cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a93bf72397614b1344bf866e7d5d35f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfa0920e82dec0f046e5",
+    "url": "/static/css/main.b177d65c.chunk.css"
+  },
+  {
+    "revision": "8c6b8c2158d3dcb76fa8",
+    "url": "/static/js/2.a7c63034.chunk.js"
+  },
+  {
+    "revision": "cfa0920e82dec0f046e5",
+    "url": "/static/js/main.17207948.chunk.js"
+  },
+  {
+    "revision": "cea6dd0cedfd46cca495",
+    "url": "/static/js/runtime-main.178b941e.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  }
+]);
